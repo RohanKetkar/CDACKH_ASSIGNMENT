329287e18be02875cef24c400307c54fececf749
Rohan Ketkar <yourgitemail@example.com>
2024-08-21 15:03:34 +0530

    1

---
+++ assignment1/edabit/hourtosecond.java
@@ -0,0 +1,58 @@
+package assignment1.edabit;
+class hourtosecond {
+
+    public static void main(String args[]){
+
+int hour=2;
+
+int ans = hourtosecondfunction(hour);//hourtosecond
+System.out.println(hour+"hour equal to "+ans+" second");
+
+int num = addbyone(hour);//addbyone
+System.out.println("num is "+num);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+System.out.println(maxedgeoftriangle(8,8));
+
+System.out.println(equal(18,28));
+    }
+static int hourtosecondfunction(int hour){
+
+int second = hour*60*60;
+
+return second;
+
+}
+
+static int addbyone(int num){
+ num = num+1;
+    return num;
+}
+
+
+
+static int maxedgeoftriangle(int num1,int num8){
+
+    return (num1+num8)-1;
+}
+
+static boolean equal(int num1,int num8){
+
+    return num1==num8;
+}
+
+}
---
+++ assignment1/edabit/index.java
@@ -0,0 +1,30 @@
+package assignment1.edabit;
+class index1{
+
+    public static void main(String args[]){
+
+        System.out.println("Working "+" abcde "+" 1" +"12345678" +"1"+"1");
+
+        System.out.println(add(10,20));
+    }
+     static int  add(int a , int b){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+System.out.print("123456781\n");
+return a+b;
+    }
+}
